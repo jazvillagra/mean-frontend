a3a6bf14e254bd1cc524886c45504a37df859a6c
Jazmin Villagra <jazmin.villagra@cycleon.com>
2022-08-23 19:51:37 +0200

    Implement POST for creation of new post

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -24,7 +24,11 @@ export class PostService{
 
   addPost(id: string, title: string, content: string) {
     const post: Post = {id: '', title: title, content: content};
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log('Response data: ', responseData);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
